8d5c5d8b72189291335896b6765765893507ed64
Ethan_Gao <e784g298@home.ku.edu>
2025-04-05 19:12:20 -0500

    Stage 3

--- include/level.h
+++ include/level.h
@@ -7,6 +7,7 @@
 
 Level tutorial_0();
 Level tutorial_1();
+Level tutorial_2();
 Level gaven_level();
 
 void level_draw(Level *level, Player *player, float dt);
--- src/level.c
+++ src/level.c
@@ -54,7 +54,9 @@ void level_draw(Level *level, Player *player, float dt) {
   switch (level->id){
     case 0: DrawTextEx(foont, "Press A or D to WALK", (Vector2){70,120},16, 1, WHITE);
       break;
-    case 1: break;
+    case 1: DrawTextEx(foont, "Press W to JUMP", (Vector2){70,120},16, 1, WHITE);
+    break;
+    case 2: DrawTextEx(foont, "Press R to RESET", (Vector2){70,120},16, 1, WHITE);
     default: break;
   }
 }
@@ -82,6 +84,9 @@ Level getLevel(int level_index) {
       return tutorial_1();
       break;
     case 2:
+      return tutorial_2();
+      break;
+    case 3:
       return gaven_level();
       break;
     default:
@@ -116,16 +121,34 @@ Level tutorial_1() {
   level.platforms[2] = (Platform){(Rectangle){256, 112, 64, 96}};
   level.platforms[3] = (Platform){(Rectangle){256-64-64, 112+48+16, 64, 96-48-16}};
   level.platforms[4] = (Platform){(Rectangle){0, 0, 320, 32}};
-  level.transition[0] = (Transition){(Rectangle){318, 160, 1000, 48}, 0};
+  level.transition[0] = (Transition){(Rectangle){318, 100, 1000, 100}, 2};
 
   level.transition_count = 1;
   level.platform_count = 5;
   return level;
 }
 
+Level tutorial_2() {
+  Level level = {0};
+  level.id = 2;
+  level.startingPosition = (Vector2){50, 185};
+  level.platforms[0] = (Platform){(Rectangle){0, 208, 6*16, 32}};
+  level.platforms[1] = (Platform){(Rectangle){0, 0, 2, 208}};
+  level.platforms[2] = (Platform){(Rectangle){318, 0, 2, 160}};
+  level.platforms[3] = (Platform){(Rectangle){0, 0, 320, 32}};
+  level.transition[0] = (Transition){(Rectangle){318, 160, 1000, 48}, 0};
+
+  level.platforms[4] = (Platform){(Rectangle){14*16, 208, 10*16, 32}};
+
+  level.transition_count = 1;
+  level.platform_count = 5;
+  
+  return level;
+}
+
 Level gaven_level() {
   Level level = {0};
-
+  level.id = 6969;
   level.startingPosition = (Vector2){50, 20};
   
   level.platforms[0] = (Platform){(Rectangle){0, 208, 320, 32}};
